b1c9b95cf385af64810e1697e92b3c4e1ba685c8
aSurgingRiver <aSurgingRiver@163.com>
2021-12-18 12:48:43 +0800

    4.26

--- Source/WebView/WebView.Build.cs
+++ Source/WebView/WebView.Build.cs
@@ -1,5 +1,13 @@
 // Copyright aSurgingRiver, Inc. All Rights Reserved.
 
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO.Compression;
+using System.Text;
+
+
 namespace UnrealBuildTool.Rules
 {
     public class WebView : ModuleRules
@@ -7,9 +15,33 @@ namespace UnrealBuildTool.Rules
         public WebView(ReadOnlyTargetRules Target) : base(Target)
         {
             bool isUsingJson = false;
-            if (isUsingJson)  {// use for JsonLibaray
+            if (isUsingJson)
+            {// use for JsonLibaray
                 PublicDefinitions.Add("JSON_LIB"); //添加 自定义的宏 或者 引擎的宏
                 PublicDependencyModuleNames.Add("JsonLibrary");
+            }
+            string RootPath = ModuleDirectory;
+            string subfix = ".template";
+            foreach (string filePath in Directory.EnumerateFiles(RootPath, "*"+subfix, SearchOption.AllDirectories))
+            {
+                string FileName = Path.GetFileName(filePath);
+                string pathDst = filePath.Replace(FileName, FileName.Replace(subfix, ""));
+                string srcContent;
+                srcContent = File.ReadAllText(filePath);
+                if (isUsingJson) {// 生存新的文件
+                    srcContent = srcContent.Replace("//@TEMPLATE","");
+                }
+                if (!File.Exists(pathDst)) {// 不存在直接写入
+                    //FileStream stream=File.Open(pathDst, FileMode.Truncate);
+                    File.WriteAllText(pathDst, srcContent);
+                    continue;
+                }
+                // 存在则判断内容是否一致
+                string dstContent = File.ReadAllText(pathDst);
+                if (GetMd5Hash(srcContent) == GetMd5Hash(dstContent)) {
+                    continue;
+                }
+                File.WriteAllText(pathDst, srcContent);
             }
 
             PublicDependencyModuleNames.AddRange(
@@ -36,6 +68,19 @@ namespace UnrealBuildTool.Rules
                     }
                 );
             }
+        }
+
+        public string GetMd5Hash(string input){
+            if (input == null) {
+                return null;
+            }
+            MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++) {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
         }
     }
 }
