a601d54323d3b499e32e513088fcf741a977d200
aSurgingRiver <aSurgingRiver@163.com>
2022-03-12 12:41:26 +0800

    UE5 预览版2
    版本升级

---
+++ Source/WebView/WebView.Build.cs
@@ -0,0 +1,86 @@
+// Copyright aSurgingRiver, Inc. All Rights Reserved.
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO.Compression;
+using System.Text;
+
+
+namespace UnrealBuildTool.Rules
+{
+    public class WebView : ModuleRules
+    {
+        public WebView(ReadOnlyTargetRules Target) : base(Target)
+        {
+            bool isUsingJson = false;
+            if (isUsingJson)
+            {// use for JsonLibaray
+                PublicDefinitions.Add("JSON_LIB"); //添加 自定义的宏 或者 引擎的宏
+                PublicDependencyModuleNames.Add("JsonLibrary");
+            }
+            string RootPath = ModuleDirectory;
+            string subfix = ".template";
+            foreach (string filePath in Directory.EnumerateFiles(RootPath, "*"+subfix, SearchOption.AllDirectories))
+            {
+                string FileName = Path.GetFileName(filePath);
+                string pathDst = filePath.Replace(FileName, FileName.Replace(subfix, ""));
+                string srcContent;
+                srcContent = File.ReadAllText(filePath);
+                if (isUsingJson) {// 生存新的文件
+                    srcContent = srcContent.Replace("//@TEMPLATE","");
+                }
+                if (!File.Exists(pathDst)) {// 不存在直接写入
+                    //FileStream stream=File.Open(pathDst, FileMode.Truncate);
+                    File.WriteAllText(pathDst, srcContent);
+                    continue;
+                }
+                // 存在则判断内容是否一致
+                string dstContent = File.ReadAllText(pathDst);
+                if (srcContent.GetHashCode() == dstContent.GetHashCode()) {
+                    continue;
+                }
+                File.WriteAllText(pathDst, srcContent);
+            }
+
+            PublicDependencyModuleNames.AddRange(
+                new string[]{
+                    "Core",
+                    "CoreUObject",
+                    "Slate",
+                    "SlateCore",
+                    "UMG",
+                    "Engine",
+                    "CefBrowser"
+                }
+            );
+
+            if (Target.bBuildEditor == true) {
+                PrivateIncludePathModuleNames.AddRange(
+                    new string[] {
+                        "UnrealEd",
+                    }
+                );
+                PrivateDependencyModuleNames.AddRange(
+                    new string[] {
+                        "UnrealEd",
+                    }
+                );
+            }
+        }
+
+        //public string GetMd5Hash(string input){
+        //    if (input == null) {
+        //        return null;
+        //    }
+        //    MD5 md5Hash = MD5.Create();
+        //    byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+        //    StringBuilder sBuilder = new StringBuilder();
+        //    for (int i = 0; i < data.Length; i++) {
+        //        sBuilder.Append(data[i].ToString("x2"));
+        //    }
+        //    return sBuilder.ToString();
+        //}
+    }
+}
